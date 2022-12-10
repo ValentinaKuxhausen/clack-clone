6e15a5412c822f7bec7e0bc09bc86e3d5e588c53
Stefan Herrmann <99035284+StefanHerrmann1991@users.noreply.github.com>
2022-12-10 08:00:13 +0100

    installing firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'slack-clone-904c2',
+    appId: '1:508888231791:web:59a749ede68767083bc29a',
+    storageBucket: 'slack-clone-904c2.appspot.com',
+    apiKey: 'AIzaSyCitqqyGhthjAIyRfgn9R0-fMi2Wg0sjf8',
+    authDomain: 'slack-clone-904c2.firebaseapp.com',
+    messagingSenderId: '508888231791',
+  },
   production: false
 };
