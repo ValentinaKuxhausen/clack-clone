c0bd4331b1f9c4b934140a96abae77ad2546aea3
Stefan Herrmann <99035284+StefanHerrmann1991@users.noreply.github.com>
2023-01-09 13:49:28 +0100

    bug with tree

--- src/app/navbar/navbar-channels/navbar-channels.component.ts
+++ src/app/navbar/navbar-channels/navbar-channels.component.ts
@@ -2,17 +2,44 @@ import { Component } from '@angular/core';
 import { Channel } from 'src/models/channel.class';
 import { MatDialog } from '@angular/material/dialog';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { FlatTreeControl } from '@angular/cdk/tree';
+import { MatTreeFlatDataSource, MatTreeFlattener } from '@angular/material/tree';
 
 @Component({
   selector: 'app-navbar-channels',
   templateUrl: './navbar-channels.component.html',
   styleUrls: ['./navbar-channels.component.sass']
 })
+
+
 export class NavbarChannelsComponent {
+  constructor(private firestore: AngularFirestore, public dialog: MatDialog, private TreeControl: FlatTreeControl, ) { }
 
-  constructor(private firestore: AngularFirestore, public dialog: MatDialog) { }
 
+  channelsRef = this.firestore.collection('channels');
   channel: Channel = new Channel();
   allChannels = [];
+  themes = [];
+  name: string;
+  TREE_DATA: Themes[];
+  level: number;
+  expandable: boolean;
+  dataSource;
+
+
+
+  
+  ngOnInit() {
+    this.dataSource.data = this.themes;
+    this.channelsRef.get().subscribe(snapshot => {
+      snapshot.forEach(doc => {
+        const channel = new Channel(doc.data()); // create a new Channel object from the doc data
+        const theme = channel.theme;
+        this.themes.push(theme); // add each theme to the array
+      });
+    });
+  }
+  
+
 
 }
