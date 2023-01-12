d54ebe45420a2f46523ea30a082495bcd882a7c5
Valentina Kuxhausen <valentina.kuxhausen@web.de>
2023-01-12 10:53:20 +0100

    userEditProfileDialog

--- src/app/user-edit-profile-dialog/user-edit-profile-dialog.component.ts
+++ src/app/user-edit-profile-dialog/user-edit-profile-dialog.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { FormBuilder } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { User } from 'src/models/user.class';
+import { AuthService } from '../shared/services/auth.service';
 
 @Component({
   selector: 'app-user-edit-profile-dialog',
@@ -7,4 +12,25 @@ import { Component } from '@angular/core';
 })
 export class UserEditProfileDialogComponent {
 
+  user: User = new User();
+  userId = '';
+  dialog: any;
+
+  constructor(
+    public authService: AuthService,
+    private firestore: AngularFirestore,
+    public dialogRef: MatDialogRef<UserEditProfileDialogComponent>, 
+    private _formBuilder: FormBuilder
+    )  { }
+
+    getUser() {
+      this.firestore
+        .collection('users')
+        .doc(this.userId)
+        .valueChanges()
+        .subscribe((user: any) => {
+          this.user = new User(user);
+        })
+    }
+  
 }
